329b6f676c2caee0c3d77b10158eaed529b81021
DennisAqua <dbwire00@gmail.com>
2022-06-30 20:08:21 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
-#!/bin/bash
 #include <stdio.h>
+
 /**
  *  * main - Entry point
- *   * print word to output
+ *   * print to output
  *    * Return: Always 0 (Success)
  *     */
+
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle"); return (0)}
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
+}
