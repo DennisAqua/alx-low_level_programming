3217555ea455cc3442a4704c18ecfe8bd7fd534b
DennisAqua <107093963+DennisAqua@users.noreply.github.com>
2022-07-06 02:31:45 +0300

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,23 +1,26 @@
 #include "main.h"
 /**
- * print_last_digit - print the last digit
- *
- * @n: parameter
- *
- * Return: return the last digit
+ * jack_bauer - function
  */
-int print_last_digit(int n)
+void jack_bauer(void)
 {
-	int d = n % 10;
+	int hour, h1, h2, min, m1, m2;
 
-	if (d < 0)
+	for (hour = 0; hour <= 23; ++hour)
 	{
-		_putchar(-d + '0');
-		return (-d);
-	}
-	else
-	{
-		_putchar(d + '0');
-		return (d);
+		h1 = hour / 10;
+		h2 = hour % 10;
+		for (min = 0; min < 60; ++min)
+		{
+			m1 = min / 10;
+			m2 = min % 10;
+			_putchar(h1 + '0');
+			_putchar(h2 + '0');
+			_putchar(':');
+			_putchar(m1 + '0');
+			_putchar(m2 + '0');
+			_putchar(10);
+		}
+		min = 0;
 	}
 }
