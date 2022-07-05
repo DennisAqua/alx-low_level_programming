59dd1d816a78c5e71f3248b862aee94f60a79595
DennisAqua <107093963+DennisAqua@users.noreply.github.com>
2022-07-06 02:11:34 +0300

    Create 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _islower - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _islower(int c)
+{
+int i;
+
+if ((c >= 97 && c <= 122) || (c >= 48 && c <= 57))
+{
+i = 1;
+}
+else
+{
+i = 0;
+}
+return (i);
+}
