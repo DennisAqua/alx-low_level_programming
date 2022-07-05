53c52c6af1970a665854ad2164b45cea663f0616
DennisAqua <107093963+DennisAqua@users.noreply.github.com>
2022-07-06 02:13:39 +0300

    Create 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _isalpha - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _isalpha(int c)
+{
+int i;
+if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+{
+i = 1;
+}
+else
+{
+i = 0;
+}
+return (i);
+}
