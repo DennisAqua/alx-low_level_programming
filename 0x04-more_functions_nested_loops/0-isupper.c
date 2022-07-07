05849660eaa3cda0adf8efd8dfd497426223b2db
DennisAqua <107093963+DennisAqua@users.noreply.github.com>
2022-07-08 01:44:54 +0300

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,22 +1,13 @@
 #include "main.h"
 
 /**
- * _isupper - writes the character c to stdout
- * @c: The character to print
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * _isupper - function that checks for uppercaseor lowercase
+ * @c: int type number
+ * Return: 1 if uppercase, else 0
  */
 int _isupper(int c)
 {
-int i;
-
-if ((c >= 65 && c <= 90))
-{
-i = 1;
-}
-else
-{
-i = 0;
-}
-return (i);
+	if (c > 64 && c < 91)
+		return (1);
+	return (0);
 }
