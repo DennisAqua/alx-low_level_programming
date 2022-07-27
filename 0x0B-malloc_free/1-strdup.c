63dfac08221d3969cb836e4b2213cacf40e77206
Dennis Mkalama <107093963+DennisAqua@users.noreply.github.com>
2022-07-28 00:29:57 +0300

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,23 +1,32 @@
-#include "main.h"
+#include <stdlib.h>
 /**
- * _strdup - function pointing to new copied memory allocation
- * @str: string to be copied
- * Return: pointer to new memory location, Null otherwise
- **/
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * @str: char
+ * Description:  contains a copy of the string given as a parameter.
+ * Return: pointer to char
+ */
 char *_strdup(char *str)
 {
-	char *cpy;
-	int len = 0;
-	int i;
+char *ptrStr;
+int  i;
 
-	if (str == NULL)
-		return (NULL);
-	while (str[len])
-		len++;
-	cpy = malloc(sizeof(char) * (++len));
-	if (cpy == NULL)
-		return (NULL);
-	for (i = 0; i < len - 1; i++)
-		cpy[i] = str[i];
-	return (cpy);
+if (str ==  NULL)
+return (NULL);
+
+for (i = 0; str[i] != '\0' ; i++)
+;
+
+ptrStr =  malloc((i + 1) * sizeof(char));
+
+if (ptrStr == NULL)
+return (NULL);
+
+for (i = 0; str[i] != '\0' ; i++)
+{
+ptrStr[i] = str[i];
+}
+
+ptrStr[i] = '\0';
+
+return (ptrStr);
 }
