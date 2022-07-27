5e3fe856cc1f40114bc2945fab17a6d2a8e447f8
Dennis Mkalama <107093963+DennisAqua@users.noreply.github.com>
2022-07-28 00:35:01 +0300

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,38 +1,42 @@
+#include "holberton.h"
 #include <stdlib.h>
+
 /**
- * str_concat - concatenates two strings.
- * @s1: string 1
- * @s2: string 2
- * Description:  concatenates two strings..
- * Return: pointer to char
+ * str_concat - Concatenates two strings.
+ * @s1: The string to be concatenated upon.
+ * @s2: The string to be concatenated to s1.
+ *
+ * Return: If concatenation fails - NULL.
+ *         Otherwise - a pointer the newly-allocated space in memory
+ *                     containing the concatenated strings.
  */
 char *str_concat(char *s1, char *s2)
 {
-  char *ptrStr;
-  int  i,lenCad = 0;
+	char *concat_str;
+	int index, concat_index = 0, len = 0;
 
-  if (s1 ==  NULL && s1 ==  NULL)
-    {
-      ptrStr = "";
-      return (ptrStr);
-    }
+	if (s1 == NULL)
+		s1 = "";
 
-  if (s1 !=  NULL )
-    {
-      for (i = 0; s1[i] != '\0' ; i++)
-	lenCad++;
-    }
+	if (s2 == NULL)
+		s2 = "";
 
-  if (s2 !=  NULL )
-    {
-      for (i = 0; s2[i] != '\0' ; i++)
-	lenCad++;
-    }
+	for (index = 0; s1[index] || s2[index]; index++)
+		len++;
 
-  ptrStr =  malloc((lenCad + 1) * sizeof(char));
+	concat_str = malloc(sizeof(char) * len);
 
-  if (ptrStr == NULL)
-    return (NULL);
+	if (concat_str == NULL)
+		return (NULL);
+
+	for (index = 0; s1[index]; index++)
+		concat_str[concat_index++] = s1[index];
+
+	for (index = 0; s2[index]; index++)
+		concat_str[concat_index++] = s2[index];
+
+	return (concat_str);
+}
 
   for (i = 0; s1[i] != '\0' ; i++)
     {
