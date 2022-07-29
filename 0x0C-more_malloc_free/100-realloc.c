3dccb81377fb77c61099a54af7eed71070f542e7
Dennis Mkalama <107093963+DennisAqua@users.noreply.github.com>
2022-07-30 01:56:17 +0300

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,38 +1,49 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
 /**
- * _realloc -  reallocates a memory block using malloc and free
- * @ptr: pointer
- * @old_size: old size
- * @new_size: new size
- * Return: pointer
+ * _realloc - function that creates an array of integers.
+ * @ptr: number of elements
+ * @old_size: old size of element
+ * @new_size: new size to assig
+ * Description: function that creates an array of integers.
+ * Return: pointer with new size
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *clone, *relloc;
-	unsigned int i;
+char *nptr, *p;
+unsigned int c;
 
-	if (ptr != NULL)
-	clone = ptr;
-	else
-	{ return (malloc(new_size)); }
-	if (new_size == old_size)
-	return (ptr);
-	if (new_size == 0 && ptr != NULL)
-	{ free(ptr);
-	return (0); }
-	relloc = malloc(new_size);
-	if (relloc == NULL)
-	return (0);
-	for (i = 0; i < (old_size || i < new_size); i++)
-	{
-		*(relloc + i) = clone[i];
-	}
-	free(ptr);
-return (relloc);
+if (old_size == new_size)
+return (ptr);
+
+if (new_size == 0 && ptr != NULL)
+{
+free(ptr);
+return (NULL);
+}
+else if (ptr == NULL && old_size != 0)
+new_size = old_size;
+
+p = ptr;
+nptr = malloc(sizeof(char) * new_size);
+
+if (nptr == NULL)
+return (NULL);
+
+if (p == NULL)
+return (nptr);
+
+if (new_size > old_size)
+{
+for (c = 0 ; c <= old_size ; c++)
+nptr[c] = p[c];
+}
+else
+{
+for (c = 0 ; c < new_size ; c++)
+nptr[c] = p[c];
+}
+free(p);
+return (nptr);
 }
 for (c = 0 ; c <= old_size ; c++)
 nptr[c] = p[c];
