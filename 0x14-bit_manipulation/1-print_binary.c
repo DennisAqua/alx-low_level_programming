01e528ec42752cf1dde13f6811afe03ddad2ff03
Dennis Mkalama <107093963+DennisAqua@users.noreply.github.com>
2022-08-20 01:36:39 +0300

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,37 +1,18 @@
+/*
+ * File: 1-print_binary.c
+ * Auth: Brennan D Baraban
+ */
+
 #include "holberton.h"
+
 /**
- * print_binary - prints the binary representation.
- * @n: number
- * Description: prints the binary representation of a number
- * Return: nothing
+ * print_binary - Prints the binary representation of a number.
+ * @n: The number to be printed in binary.
  */
 void print_binary(unsigned long int n)
 {
-unsigned long int m;
-int flag = 0;
+	if (n > 1)
+		print_binary(n >> 1);
 
-if (n == 0)
-{
-_putchar('0');
-return;
-}
-if (n == 1)
-{
-_putchar('1');
-return;
-}
-
-for (m = 1UL << 63; m > 0; m = m >> 1)
-{
-if (n & m)
-{
-_putchar('1');
-flag = 1;
-}
-else
-{
-if (flag)
-_putchar('0');
-}
-}
+	_putchar((n & 1) + '0');
 }
